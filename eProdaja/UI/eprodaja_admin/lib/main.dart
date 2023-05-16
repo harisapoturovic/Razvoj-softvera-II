08aa17e298f7f5cb7930aa3db9e891aa2853f5af
Harisa <harisa.poturovic@edu.fit.ba>
2023-05-17 00:28:17 +0200

    V8

---
+++ eProdaja/UI/eprodaja_admin/lib/main.dart
@@ -0,0 +1,185 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyMaterialApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const LayoutWidget()
+    );
+  }
+}
+
+class MyAppBar extends StatelessWidget {
+  String title;
+  MyAppBar({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(title);
+  }
+}
+
+class Counter extends StatefulWidget {
+  const Counter({super.key});
+
+  @override
+  State<Counter> createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int _count = 0;
+
+  void _incrementCounter(){
+    setState(() {      
+    _count++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Text("You have pushed button $_count times"),
+      ElevatedButton(onPressed: _incrementCounter, child: const Text("increment++"))
+    ],
+    );
+  }
+}
+
+class LayoutWidget extends StatelessWidget {
+  const LayoutWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Container(
+        height: 150,
+        color: Colors.red,
+        child: Center(
+          child: Container(
+                height: 100,
+                color: Colors.blue, 
+                child: Text("Example text"), 
+                alignment: Alignment.center,),
+        ),
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Text("Text1"),
+          Text("Text2"),
+          Text("Text3"),
+      ],),
+      Container(
+        height: 100,
+        color: Colors.green,
+        child: Text("Contain"),
+      ),
+    ],
+    );
+  }
+}
+
+class MyMaterialApp extends StatelessWidget {
+  const MyMaterialApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "RS II Material App",
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: LoginPage()
+      /*Scaffold(
+        appBar: AppBar(
+          title: const Text("RS II desktop app"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const TextField(
+              decoration: InputDecoration(
+                labelText: "Enter your name"
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(onPressed: (){
+              print("Button clicked");
+            }, 
+            child: const Text("Submit"))
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: () {},
+      child: Icon(Icons.add),
+      ),
+    )*/
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Login"),
+      ),
+      body: Center(
+        child: Container(
+           constraints: BoxConstraints(maxHeight: 400, maxWidth: 400),
+          child: Card(
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(children: [
+                Image.network("https://www.fit.ba/content/public/images/og-image.jpg", height: 150, width: 150,),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: "Username",
+                    prefixIcon: Icon(Icons.email)
+                    ),
+                  ),
+                  SizedBox(height: 8,),
+                  TextField(
+                  decoration: InputDecoration(
+                    labelText: "Password",
+                    prefixIcon: Icon(Icons.password)
+                    ),
+                  ),
+                  SizedBox(height: 8,),
+                  ElevatedButton(onPressed: (){
+                    print("login proceed");
+                  }, child: Text("Login"))
+              ],
+              ),
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: () {},
+      child: Icon(Icons.add),
+      ),
+    );
+  }
+}
